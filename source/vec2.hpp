eee419a3e65574c810bc71cb02370c859dafdb58
mathis0305 <kuenzelmathis@gmx.de>
2021-05-01 12:10:49 +0100

    add struct declaration Vec2 to vec2.hpp

--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,10 +1,19 @@
 #ifndef  VEC2_HPP
 #define VEC2_HPP
 
+/*EXPLANATION include guards are commands to avoid double includes.
+  Before actually including this file they check if some of the files
+  parts were already included via previous including commands.
+  If you don't do this it is possible to include the struct Vec2 two
+  times and this is a double declaration. It would lead to a compile
+  error. */
+
 // Vec2 data type definition
 struct Vec2 {
 	/*TODO add member variables with
 	  default member initialisation */
+	float x = 0.0f;
+	float y = 0.0f;
 };
 
 #endif // VEC2_HPP
