0ff2f04b733fa61608533bd3d1fa3d91aced1d69
mathis0305 <kuenzelmathis@gmx.de>
2021-05-01 12:13:37 +0100

    add tests for vec2 structs

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,14 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.cpp"
 
-int main(int argc, char *argv[])
-{
+TEST_CASE("test_struct_vec2", "[vec2]") {
+	REQUIRE(a.x == 0.0f);
+	REQUIRE(a.y == 0.0f);
+	REQUIRE(b.x == 5.1f);
+	REQUIRE(b.y == -9.3f);
+}
+
+int main(int argc, char *argv[]) {
   return Catch::Session().run(argc, argv);
 }
